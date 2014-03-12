a4086eecaefd2fdd8ec46d1e7e492163dcacc102
Mateus Craveiro <mccraveiro@gmail.com>
2014-03-12 19:22:16 -0400

    Swipe working on main screen

---
+++ www/assets/scripts/index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var mySwiper = new Swiper('.swiper-container:nth-of-type(1)', {
+        mode:'horizontal',
+        loop: false
+    });
+
+    var mySwiper = new Swiper('.swiper-container:nth-of-type(2)', {
+        mode:'horizontal',
+        loop: false
+    });
+}

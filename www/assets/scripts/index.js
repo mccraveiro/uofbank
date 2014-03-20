8265edc3468bf336b1175c7433051d11c2158b7f
Mateus Craveiro <mccraveiro@gmail.com>
2014-03-20 17:48:39 -0400

    Login styles and initial routing system

--- www/assets/scripts/index.js
+++ www/assets/scripts/index.js
@@ -9,3 +9,38 @@ window.onload = function() {
         loop: false
     });
 }
+
+var app = angular.module('uofbank', ['ngRoute']);
+
+// configure our routes
+app.config(function($routeProvider, $locationProvider) {
+
+    $routeProvider
+		// route for the login page
+		.when('/', {
+			templateUrl : 'assets/pages/login.html',
+			controller  : 'loginController'
+		})
+		// route for the main page
+		.when('/main', {
+			templateUrl : 'assets/pages/main.html',
+			controller  : 'mainController'
+		})
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    $locationProvider.hashPrefix('!');
+});
+
+app.controller('loginController', function ($scope, $location) {
+
+    $scope.signIn = function() {
+        $location.path('/main');
+    };
+
+});
+
+app.controller('mainController', function($scope) {
+
+});
